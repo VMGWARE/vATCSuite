56b924239d25dbca34ca827a03d7624346a69dc7
Codycody31 <Github@codycody.org>
2022-11-26 22:13:51 -0500

    Reformating pages

--- includes/constants.php
+++ includes/constants.php
@@ -2,7 +2,7 @@
 error_reporting(0);
 
 /* Defining the variables for the database connection. */
-define('HOST', '');
-define('USERNAME', '');
-define('PASSWORD', '');
-define('DATABASE', '');
+define('HOST', 'cloud.fshs.info');
+define('USERNAME', 'redbeard_atis_generator');
+define('PASSWORD', 'Pul4]1ts1/QsflqJ');
+define('DATABASE', 'atis-generator');
