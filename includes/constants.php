7606547498eb5ef2f6fc8f0d3d02e306e0f4ca5c
Codycody31 <Github@codycody.org>
2023-01-10 13:04:54 -0500

    Removed faulty db connection info

--- includes/constants.php
+++ includes/constants.php
@@ -2,7 +2,7 @@
 error_reporting(0);
 
 /* Defining the variables for the database connection. */
-define('HOST', 'cloud.fshs.info');
-define('USERNAME', 'redbeard_atis_generator');
-define('PASSWORD', 'Pul4]1ts1/QsflqJ');
-define('DATABASE', 'atis-generator');
+define('HOST', 'example');
+define('USERNAME', 'example');
+define('PASSWORD', 'example');
+define('DATABASE', 'example');
