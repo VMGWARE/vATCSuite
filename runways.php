d99aeb2e0e38fb5acbdc6b6ad5d93d7c2cdb8545
Codycody31 <Github@codycody.org>
2023-03-26 02:15:51 -0400

    Caught issue with connecting to DB

--- runways.php
+++ runways.php
@@ -5,6 +5,33 @@ if (!isset($_POST["icao"])) {
     return false;
 }
 
+// Check mysql connection
+$mysqli = null;
+try {
+    $mysqli = new mysqli(constant('HOST'), constant('USERNAME'), constant('PASSWORD'), constant('DATABASE'));
+} catch (Exception $e) {
+    echo '
+        <div class="modal fade" id="runway-modal" tabindex="-1">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Oops!</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                    </div>
+                    <div class="modal-body text-center">
+                        <p class="fs-3 text-danger"><i class="fa-solid fa-circle-xmark"></i><br/>Connection Failed</p>
+                        <p>Failed to connect to database, please contact the administrator.</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    ';
+    return false;
+}
+
 /**
  * It connects to the database, queries the database for the runways of the airport, and returns the
  * result
