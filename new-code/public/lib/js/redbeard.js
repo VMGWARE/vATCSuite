17e14f71132a9f75c2819f74a1a9baee22f7e392
Vahn Gomes <Github@codycody.org>
2023-05-12 12:38:12 -0400

    Still working on it

--- new-code/public/lib/js/redbeard.js
+++ new-code/public/lib/js/redbeard.js
@@ -12,23 +12,32 @@ $(document).ready(function () {
         }
     });
 
-    function i(t) {
-        output = $(t).html();
+    /**
+     * Copy text to clipboard
+     * @param {string} text - The text to copy
+     * @returns {void} Nothing
+     */
+    function copy(text) {
+        output = $(text).html();
         navigator.clipboard.writeText(output)
     }
+
     $("#list-runways").click(function (t) {
         t.preventDefault();
         $(".loading").show();
-        $.post("runways.php", $("#atis-input").serialize(), function (t) {
+        icao = $("#icao").val();
+        $.get(`/api/v1/airport/${icao}/runways`, function (t) {
             $("#runway-output").html(t);
             $("#runway-modal").modal("show");
             $(".loading").hide()
         })
     });
+
     $("#atis-input").submit(function (t) {
         $(".loading").show();
         t.preventDefault();
-        $.post("atis.php", $("#atis-input").serialize(), function (t) {
+        icao = $("#icao").val();
+        $.get(`/api/v1/airport/${icao}/atis`, $("#atis-input").serialize(), function (t) {
             $("#atis-output").html(t);
             $("#atis-modal").modal("show");
             $(".loading").hide();
@@ -37,17 +46,24 @@ $(document).ready(function () {
             ident = $("#ident").val();
             $("#download-atis").attr("href", "tts.php?atis=" + atis2 + "&icao=" + icao + "&ident=" + ident);
             $("#copy-atis").click(function () {
-                i("#atis1")
+                copy("#atis1")
             })
         })
     });
+
     $("#squawk-generator").click(function () {
         $("#squawk-modal").modal("show")
     });
 
+    /**
+     * Generate a random number between 1 and 7
+     *
+     * @returns {number} Random number
+     */
     function getRandomInt() {
         return Math.floor(Math.random() * 7 + 1)
     }
+
     $("#generate-squawk").click(function () {
         code = "";
         code = code.concat(3);
@@ -57,7 +73,8 @@ $(document).ready(function () {
         $("#copy-squawk").show();
         $("#squawk-output").html(code)
     });
+
     $("#copy-squawk").click(function () {
-        i("#squawk-output")
+        copy("#squawk-output")
     })
 });
