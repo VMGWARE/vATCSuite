2cf106154f4c2f287c46b2c785e369e0f4a69675
Vahn Gomes <Github@codycody.org>
2023-05-11 23:13:18 -0400

    Switched to using layout

--- new-code/resources/views/home.blade.php
+++ new-code/resources/views/home.blade.php
@@ -1,22 +1,6 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.master')
 
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link href="{{ asset('/lib/css/overrides.css') }}" rel="stylesheet">
-
-    <script src="https://kit.fontawesome.com/6a94b5c85f.js" crossorigin="anonymous"></script>
-
-    <title>Redbeard's ATIS Generator</title>
-</head>
-
-<body>
+@section('content')
     <div class="loading hide">
         <div class="position-absolute top-50 start-50">
             <div class="spinner-border text-light" role="status"></div>
@@ -37,8 +21,7 @@
                 <div class="row">
                     <div class="col">
                         <label for="icao" class="form-label">1. icao</label>
-                        <input type="text" maxlength="4" class="form-control" id="icao" name="icao"
-                            required>
+                        <input type="text" maxlength="4" class="form-control" id="icao" name="icao" required>
                     </div>
                     <div class="col-auto">
                         <div><label for="list-runways" class="form-label">2. Get Runways</label></div>
@@ -173,16 +156,4 @@
             </div>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
-        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
-        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
-    </script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="{{ asset('/lib/js/redbeard.js') }}"
-        integrity="sha384-iJkOZ98Q2+eS2mEcOzRcyxAlswxPN5/x3BtTsLG4g7eEpuM0eWXMwmaT3qit/OCN" crossorigin="anonymous">
-    </script>
-</body>
-
-</html>
+@stop
