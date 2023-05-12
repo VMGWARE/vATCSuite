b6ec7ec981b79206c6eb0f7dd8b35e691468044e
Vahn Gomes <Github@codycody.org>
2023-05-12 12:37:53 -0400

    Added airports table

---
+++ new-code/app/Models/Airport.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Airport extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'icao',
+        'iata',
+        'name',
+        'runways',
+    ];
+
+    protected $casts = [
+        'runways' => 'array',
+    ];
+}
