aa7c478d09d037010d6bf06f1301e958c681a490
Vahn Gomes <Github@codycody.org>
2023-05-25 12:47:54 -0400

    🔧chore(config/app.php): add voice-rss api key configuration to app name configuration

--- new-code/config/app.php
+++ new-code/config/app.php
@@ -17,6 +17,7 @@ return [
     */
 
     'name' => env('APP_NAME', 'Laravel'),
+    'voice-rss-key' => env('VOICE_RSS_API_KEY', 'e1f2f3f4f5f6f7f8'),
 
     /*
     |--------------------------------------------------------------------------
