dbb9c5d109fffc9dcfbc7bdb82570f6910e50af9
Vahn Gomes <Github@codycody.org>
2023-05-25 17:08:43 -0400

    🔥 chore(AirportController.php): remove unused import statement 🚀 feat(AirportController.php): add return type hints to all methods for better code clarity and maintainability 🐛 fix(AirportController.php): fix incorrect return type in textToSpeechStore method signature 🚀 feat(AirportController.php): add support for returning JSON response in textToSpeechStore method

--- new-code/app/Http/Controllers/AirportController.php
+++ new-code/app/Http/Controllers/AirportController.php
@@ -2,14 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use \App\Models\Airport as AirportModel;
 use \App\Models\ATISAudioFile;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Cache;
 use \App\Custom\AtisGenerator;
 use \App\Custom\Helpers;
 use Vyuldashev\LaravelOpenApi\Attributes as OpenApi;
-// Use Storage;
 use Illuminate\Support\Facades\Storage;
 
 #[OpenApi\PathItem]
@@ -17,15 +18,16 @@ class AirportController extends Controller
 {
     /**
      * Get Airport.
-     * 
+     *
      * Gets an airport from the database and returns it in a JSON response.
-     * 
+     *
      * @param string $icao The ICAO code of the airport to get.
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return JsonResponse
      */
     #[OpenApi\Operation(tags: ['Airport'])]
     #[OpenApi\Parameters(factory: \App\OpenApi\Parameters\GetAirportParameters::class)]
-    public function index($icao, Request $request)
+    public function index(string $icao, Request $request): JsonResponse
     {
         if (!Helpers::validateIcao($icao)) {
             return response()->json([
@@ -70,11 +72,13 @@ class AirportController extends Controller
 
     /**
      * Get All Airports.
-     * 
+     *
      * Gets all airports in the database and returns them in a JSON response.
+     * @param Request $request
+     * @return JsonResponse
      */
     #[OpenApi\Operation(tags: ['Airport'])]
-    public function all(Request $request)
+    public function all(Request $request): JsonResponse
     {
         $airports = AirportModel::all()->makeHidden(['created_at', 'updated_at']);
         return response()->json([
@@ -89,96 +93,71 @@ class AirportController extends Controller
 
     /**
      * Get Airport Runways.
-     * 
+     *
      * Gets the runways for an airport and returns them in a JSON response.
-     * 
+     *
      * @param string $icao The ICAO code of the airport to get runways for.
-     * @return \Illuminate\Http\Response
+     * @return JsonResponse
      */
     #[OpenApi\Operation(tags: ['Airport'])]
     #[OpenApi\Parameters(factory: \App\OpenApi\Parameters\GetAirportParameters::class)]
-    public function runways($icao)
+    public function runways(string $icao): JsonResponse
     {
         if (!Helpers::validateIcao($icao)) {
-            if (request()->query('res') == 'html') {
-                return view('partials.failed', [
-                    'message' => 'Invalid ICAO code.',
-                ]);
-            } else {
-                return response()->json([
-                    'status' => 'error',
-                    'message' => 'Invalid ICAO code.',
-                    'code' => 400,
-                    'data' => null
-                ]);
-            }
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Invalid ICAO code.',
+                'code' => 400,
+                'data' => null
+            ]);
         }
 
         $airport = AirportModel::where('icao', strtoupper($icao))->first();
 
         if (!$airport) {
-            if (request()->query('res') == 'html') {
-                return view('partials.failed', [
-                    'message' => 'Could not find airport with ICAO code <strong>' . strtoupper($icao) . '</strong>.',
-                ]);
-            } else {
-                return response()->json([
-                    'status' => 'error',
-                    'message' => 'Could not find airport with ICAO code ' . strtoupper($icao) . '.',
-                    'code' => 404,
-                    'data' => null
-                ]);
-            }
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Could not find airport with ICAO code ' . strtoupper($icao) . '.',
+                'code' => 404,
+                'data' => null
+            ]);
         }
 
         $metar = Helpers::fetch_metar($icao);
         if ($metar == null) {
-            if (request()->query('res') == 'html') {
-                return view('partials.failed', [
-                    'message' => 'AviationWeather.gov does not have any weather information available for <strong>' . strtoupper($icao) . '</strong>. Please try again with a different airport.',
-                ]);
-            } else {
-                return response()->json([
-                    'status' => 'error',
-                    'message' => 'Could not find METAR data for ' . strtoupper($icao) . '.',
-                    'code' => 404,
-                    'data' => null
-                ]);
-            }
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Could not find METAR data for ' . strtoupper($icao) . '.',
+                'code' => 404,
+                'data' => null
+            ]);
+
         }
         $wind = Helpers::get_wind($metar);
         $runways = Helpers::parse_runways($icao, $wind['dir']);
 
-        if (request()->query('res') == 'html') {
-            return view('partials.runways', [
-                'airport' => $airport,
-                'metar' => $metar,
-                'wind' => $wind,
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Runways retrieved successfully.',
+            'code' => 200,
+            'data' => [
                 'runways' => $runways,
-            ]);
-        } else {
-            return response()->json([
-                'status' => 'success',
-                'message' => 'Runways retrieved successfully.',
-                'code' => 200,
-                'data' => [
-                    'runways' => $runways,
-                ]
-            ]);
-        }
+            ]
+        ]);
     }
 
     /**
      * Get Airport ATIS.
-     * 
+     *
      * Gets the ATIS for an airport and returns it in a JSON response.
-     * 
+     *
      * @param string $icao The ICAO code of the airport to get the ATIS for.
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return JsonResponse
      */
     #[OpenApi\Operation(tags: ['Airport'])]
     #[OpenApi\Parameters(factory: \App\OpenApi\Parameters\GetAirportParameters::class)]
-    public function atis($icao, Request $request)
+    public function atis(string $icao, Request $request): JsonResponse
     {
         // Validate ICAO code
         if (!Helpers::validateIcao($icao)) {
@@ -268,15 +247,15 @@ class AirportController extends Controller
 
     /**
      * Get Airport METAR.
-     * 
+     *
      * Gets the METAR for an airport and returns it in a JSON response.
-     * 
+     *
      * @param string $icao The ICAO code of the airport to get the METAR for.
-     * @return \Illuminate\Http\Response
+     * @return JsonResponse
      */
     #[OpenApi\Operation(tags: ['Airport'])]
     #[OpenApi\Parameters(factory: \App\OpenApi\Parameters\GetAirportParameters::class)]
-    public function metar($icao)
+    public function metar(string $icao): JsonResponse
     {
         if (!Helpers::validateIcao($icao)) {
             return response()->json([
@@ -311,7 +290,7 @@ class AirportController extends Controller
 
     /**
      * Get Airport TTS.
-     * 
+     *
      * Gets a link to a mp3 text-to-speech file for an airport and returns it in a JSON response.
      *
      * @param string $icao The ICAO code of the airport to generate the TTS for.
@@ -320,28 +299,27 @@ class AirportController extends Controller
      */
     #[OpenApi\Operation(tags: ['Text to Speech'])]
     #[OpenApi\Parameters(factory: \App\OpenApi\Parameters\GetAirportParameters::class)]
-    public function textToSpeech($icao, Request $request)
+    public function textToSpeech(string $icao, Request $request): void
     {
         // TODO: Get mp3 atis file and return link and id.
     }
 
     /**
      * Generate Airport TTS.
-     * 
+     *
      * Generates a mp3 text-to-speech file for an airport and returns a link to it.
      *
      * @param string $icao The ICAO code of the airport to generate the TTS for.
      * @param Request $request
-     * @return void
+     * @return JsonResponse
      */
     #[OpenApi\Operation(tags: ['Text to Speech'])]
     #[OpenApi\Parameters(factory: \App\OpenApi\Parameters\GetAirportParameters::class)]
-    public function textToSpeechStore($icao, Request $request)
+    public function textToSpeechStore(string $icao, Request $request): JsonResponse
     {
         // Validate the request
         $atis = $request->atis;
         $ident = $request->ident;
-        $icao = $request->icao;
 
         if (!isset($atis) || !isset($ident) || !isset($icao)) {
             return response()->json([
@@ -421,7 +399,7 @@ class AirportController extends Controller
 
     /**
      * Delete Airport TTS.
-     * 
+     *
      * Deletes a mp3 text-to-speech file for an airport.
      *
      * @param string $icao The ICAO code of the airport to generate the TTS for.
@@ -430,7 +408,7 @@ class AirportController extends Controller
      */
     #[OpenApi\Operation(tags: ['Text to Speech'])]
     #[OpenApi\Parameters(factory: \App\OpenApi\Parameters\GetAirportParameters::class)]
-    public function textToSpeechDestroy($icao, Request $request)
+    public function textToSpeechDestroy(string $icao, Request $request): void
     {
         // TODO: Delete mp3 atis file.
     }
