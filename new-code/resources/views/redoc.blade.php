a0b573b3f2d917c1dc9576b821254e27936e421f
Vahn Gomes <Github@codycody.org>
2023-05-26 15:12:25 -0400

    🎨 style(redoc.blade.php): change page title to better reflect the content of the page

--- new-code/resources/views/redoc.blade.php
+++ new-code/resources/views/redoc.blade.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <title>Redbeard's Atis Generator Doc</title>
+    <title>Atis Generator API Documentation</title>
     <!-- needed for adaptive design -->
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
