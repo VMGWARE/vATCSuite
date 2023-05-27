f8e0354601f43a195c5218d627d8956c886e9865
Vahn Gomes <Github@codycody.org>
2023-05-27 11:37:59 -0400

    ✨ feat(redbeard.js): add ErrorModal and Modal functions to display modals with error messages and content 🐛 fix(redbeard.js): update API endpoints to match backend changes ✨ feat(redbeard.js): add success modal with ATIS text and audio download link after successful ATIS generation 🐛 fix(redbeard.js): handle error cases when generating ATIS and display error modals with appropriate messages

--- new-code/public/lib/js/redbeard.js
+++ new-code/public/lib/js/redbeard.js
@@ -22,12 +22,101 @@ $(document).ready(function () {
         navigator.clipboard.writeText(output)
     }
 
+    /**
+     * Return a modal with the error message
+     * @param message - The error message.
+     * @returns a string that represents an HTML modal element with a title, body, and a close button.
+     */
+    function ErrorModal(message, id) {
+        return `
+        <div class="modal fade show" id="${id}" tabindex="-1" aria-modal="true" role="dialog" style="display: block;">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Oops!</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                    </div>
+                    <div class="modal-body text-center">
+                        <p class="fs-3 text-danger"><i class="fa-solid fa-circle-xmark"></i><br>Generation Failed</p>
+                        <p>${message}</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `;
+    }
+
+    /**
+     * Return a modal with the title and body
+     * @param title - The title of the modal
+     * @param body - The body of the modal.
+     * @returns a string that represents an HTML modal element with a title, body, and a close button.
+     */
+    function Modal(title, body, id) {
+        return `
+        <div class="modal fade show" id="${id}" tabindex="-1" aria-modal="true" role="dialog" style="display: block;">
+            <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-lg">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">${title}</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                    </div>
+                    <div class="modal-body text-center">
+                        ${body}
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `;
+    }
+
     $("#list-runways").click(function (t) {
         t.preventDefault();
         $(".loading").show();
         icao = $("#icao").val();
-        $.get(`/api/v1/airport/${icao}/runways?res=html`, function (t) {
-            $("#runway-output").html(t);
+        $.get(`/api/v1/airports/${icao}/runways`, function (t) {
+            if (t.status == "error" || t.code != 200) {
+                $("#runway-output").html(ErrorModal(t.message, "runway-modal"));
+                $("#runway-modal").modal("show");
+                $(".loading").hide();
+                return;
+            }
+
+            // TODO: If not runways show success with overide runways button
+
+            table = `
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col"><i class="fa-solid fa-road"></i></th>
+                        <th scope="col"><i class="fa-solid fa-wind"></i></th>
+                        <th scope="col"><i class="fa-solid fa-plus-minus"></i><i class="fa-solid fa-wind"></i></th>
+                        <th scope="col"><i class="fa-solid fa-plane-arrival"></i></th>
+                        <th scope="col"><i class="fa-solid fa-plane-departure"></i></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    ${t.data.map(function (t) {
+                return `
+                        <tr>
+                            <td><strong>${t.runway}</strong></td>
+                            <td>${t.wind_dir}</td>
+                            <td>${t.wind_diff}</td>
+                            <td><input class="form-check-input" type="checkbox" name="landing_runways[]" value="${t.runway}"></td>
+                            <td><input class="form-check-input" type="checkbox" name="departing_runways[]" value="${t.runway}"></td>
+                        </tr>
+                        `
+            }).join("")}
+                </tbody>
+            </table>
+            `;
+            $("#runway-output").html(Modal("Runway List for " + icao.toUpperCase(), table, "runway-modal"));
             $("#runway-modal").modal("show");
             $(".loading").hide()
         })
@@ -37,14 +126,72 @@ $(document).ready(function () {
         $(".loading").show();
         t.preventDefault();
         icao = $("#icao").val();
-        $.post(`/api/v1/airport/${icao}/atis`, $("#atis-input").serialize(), function (t) {
-            $("#atis-output").html(t);
+        $.post(`/api/v1/airports/${icao}/atis`, $("#atis-input").serialize(), function (t) {
+            if (t.status == "error" || t.code != 200) {
+                $("#atis-output").html(ErrorModal(t.message, "atis-modal"));
+                $("#atis-modal").modal("show");
+                $(".loading").hide();
+                return;
+            }
+
+            if (t.data == "") {
+                $("#atis-output").html(ErrorModal("API returned empty data. Please try again."));
+                $("#atis-modal").modal("show");
+                $(".loading").hide();
+                return;
+            }
+
+            success = `
+            <div class="modal fade show" id="atis-modal" tabindex="-1" aria-modal="true" role="dialog" style="display: block;">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title">Success!</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                        </div>
+                        <div class="modal-body text-center">
+                            <p class="fs-3 text-success"><i class="fa-solid fa-circle-check"></i><br>Generation Success!</p>
+                            <p>Your input has been serialized, we've gotten the weather and it's all been run through the processor. It's parsed and your ATIS ready to go! Click on the buttons below to use it.</p>
+                            <div id="atis1" class="hide">${t.data.text}</div>
+                            <div id="atis2" class="hide">${t.data.spoken}</div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-primary" id="copy-atis">Copy ATIS To Clipboard</button>
+                            <a class="btn btn-primary" id="download-atis">Download .mp3</a>
+                            <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            `;
+
+            // Show success modal
+            $("#atis-output").html(success);
             $("#atis-modal").modal("show");
             $(".loading").hide();
+
             atis2 = $("#atis2").html();
             icao = $("#icao").val();
             ident = $("#ident").val();
-            $("#download-atis").attr("href", "tts.php?atis=" + atis2 + "&icao=" + icao + "&ident=" + ident);
+
+            // Make post request to /api/v1/airports/{icao}/tts
+            $.post(`/api/v1/airports/${icao}/tts`, { ident: ident, atis: atis2 }, function (t) {
+                // Show error modal if code is not 200 or 409
+                if (t.code != 200 && t.code != 409) {
+                    $("#atis-output").html(ErrorModal(t.message, "atis-modal"));
+                    $("#atis-modal").modal("show");
+                    $(".loading").hide();
+                }
+                // Show warning in console if code is 409
+                else if (t.code == 409) {
+                    console.log(`[WARN] ${t.message}`);
+                    $("#download-atis").attr("href", t.data.url).attr("download", t.data.filename);
+                }
+                // Set download link if code is 200
+                else {
+                    $("#download-atis").attr("href", t.data.url).attr("download", t.data.filename);
+                }
+            });
             $("#copy-atis").click(function () {
                 copy("#atis1")
             })
