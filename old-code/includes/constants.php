094859fdc584eed164dc6ff5bd0a88324e6ec3e6
Vahn Gomes <Github@codycody.org>
2023-05-29 19:37:26 -0400

    🔥refactor: Moved new code into root directory

--- old-code/includes/constants.php
+++
@@ -1,8 +0,0 @@
-<?php 
-error_reporting(0);
-
-/* Defining the variables for the database connection. */
-define('HOST', 'example');
-define('USERNAME', 'example');
-define('PASSWORD', 'example');
-define('DATABASE', 'example');
