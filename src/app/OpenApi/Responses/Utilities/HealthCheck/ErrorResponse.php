07dc956c6d68936a031c794a892b239151793703
Vahn Gomes <Github@codycody.org>
2023-07-24 13:41:08 -0400

    📦 chore(openapi.php): add 'Utilities' section to the OpenAPI documentation configuration file to categorize endpoints related to getting information about the API 📄 feat(api.php): add health check endpoint to the API v1 routes 🆕 feat(HealthCheckController.php): add HealthCheckController to handle API health check endpoint 🆕 feat(ErrorResponse.php): add ErrorResponse class to generate OpenAPI response schema for health check error response 🆕 feat(SuccessResponse.php): add SuccessResponse class to generate OpenAPI response schema for health check success response

---
+++ src/app/OpenApi/Responses/Utilities/HealthCheck/ErrorResponse.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\OpenApi\Responses\Utilities\HealthCheck;
+
+use GoldSpecDigital\ObjectOrientedOAS\Objects\MediaType;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\Response;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\Schema;
+use Vyuldashev\LaravelOpenApi\Factories\ResponseFactory;
+use Vyuldashev\LaravelOpenApi\Contracts\Reusable;
+
+class ErrorResponse extends ResponseFactory implements Reusable
+{
+    public function build(): Response
+    {
+        $response = Schema::object()->properties(
+            Schema::string('status')->example('error'),
+            Schema::string('message')->example('API v1 is having issues.'),
+            Schema::integer('code')->example(503),
+            Schema::object('data')->properties(
+                Schema::string('uptime')->example('3 days 2 hours'),
+                Schema::string('timestamp')->example('2021-01-01 00:00:00'),
+                Schema::string('version')->example('1.0.0'),
+                Schema::number('diskspace')->example(50.0),
+                Schema::number('latency')->example(0.123)
+            ),
+            Schema::object('dependencies')->properties(
+                Schema::string('database')->example('Error'),
+                Schema::string('storage')->example('OK'),
+            )
+        );
+
+        return Response::create('GetHealthCheckError')
+            ->description('Get API Health Check status.')
+            ->content(
+                MediaType::json()->schema($response)
+            );
+    }
+}
