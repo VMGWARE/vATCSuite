f800b16d9bd85cdf4e026bb2876b24660d5f1d76
Vahn Gomes <Github@codycody.org>
2023-07-26 14:15:27 -0400

    Remove duplicate authentication menu header

--- src/resources/views/vendor/backpack/ui/inc/menu_items.blade.php
+++ src/resources/views/vendor/backpack/ui/inc/menu_items.blade.php
@@ -7,7 +7,6 @@
 
 <!-- Users, Roles Permissions -->
 <x-backpack::menu-dropdown title="Authentication" icon="la la-lock">
-    <x-backpack::menu-dropdown-header title="Authentication" />
     <x-backpack::menu-dropdown-item title="Users" icon="la la-user" :link="backpack_url('user')" />
     <x-backpack::menu-dropdown-item title="Roles" icon="la la-group" :link="backpack_url('role')" />
     <x-backpack::menu-dropdown-item title="Permissions" icon="la la-key" :link="backpack_url('permission')" />
