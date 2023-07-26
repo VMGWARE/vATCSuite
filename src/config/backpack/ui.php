5207776c850820173b45a7f0ec0353becd055f4a
Vahn Gomes <Github@codycody.org>
2023-07-26 13:21:32 -0400

    Update UI config with home link and project logo changes

--- src/config/backpack/ui.php
+++ src/config/backpack/ui.php
@@ -60,10 +60,10 @@ return [
     // The string below will be passed through the url() helper.
     // - default: '' (project root)
     // - alternative: 'admin' (the admin's dashboard)
-    'home_link' => '',
+    'home_link' => 'admin',
 
     // Menu logo. You can replace this with an <img> tag if you have a logo.
-    'project_logo'   => '<b>vATC</b>Suite',
+    'project_logo'   => '<img src="/lib/images/icons/logo_small_icon.png" alt="vATC Suite" style="height: 35px; width: auto; margin-top: -3px; margin-right: 10px; margin-left: 10px; margin-bottom: -3px;"/><b>vATC</b> Suite',
 
     // Show / hide breadcrumbs on admin panel pages.
     'breadcrumbs' => true,
