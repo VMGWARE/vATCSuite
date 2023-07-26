9c2c2b60582500135c235aea15223887700500dc
Vahn Gomes <Github@codycody.org>
2023-07-26 12:47:37 -0400

    Refactor menu items layout for add-ons with dropdown menu

--- src/resources/views/vendor/backpack/ui/inc/menu_items.blade.php
+++ src/resources/views/vendor/backpack/ui/inc/menu_items.blade.php
@@ -6,6 +6,9 @@
 <x-backpack::menu-item title="Airports" icon="la la-plane" :link="backpack_url('airport')" />
 
 <!-- Users, Roles Permissions -->
-<x-backpack::menu-item title="Users" icon="la la-user" :link="backpack_url('user')" />
-<x-backpack::menu-item title="Roles" icon="la la-key" :link="backpack_url('role')" />
-<x-backpack::menu-item title="Permissions" icon="la la-lock" :link="backpack_url('permission')" />
+<x-backpack::menu-dropdown title="Add-ons" icon="la la-puzzle-piece">
+    <x-backpack::menu-dropdown-header title="Authentication" />
+    <x-backpack::menu-dropdown-item title="Users" icon="la la-user" :link="backpack_url('user')" />
+    <x-backpack::menu-dropdown-item title="Roles" icon="la la-group" :link="backpack_url('role')" />
+    <x-backpack::menu-dropdown-item title="Permissions" icon="la la-key" :link="backpack_url('permission')" />
+</x-backpack::menu-dropdown>
