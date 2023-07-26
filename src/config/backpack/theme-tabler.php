461e3aabba9d21471afeb65fd0acfd07b3a70a2b
Vahn Gomes <Github@codycody.org>
2023-07-26 14:59:49 -0400

    Update tabler theme layout to vertical_dark

--- src/config/backpack/theme-tabler.php
+++ src/config/backpack/theme-tabler.php
@@ -22,7 +22,7 @@ return [
      * Possible values: horizontal, horizontal_dark, horizontal_overlap, vertical,
      * vertical_dark, vertical_transparent (legacy theme), right_vertical, right_vertical_dark, right_vertical_transparent
      */
-    'layout' => 'horizontal_overlap',
+    'layout' => 'vertical_dark',
 
     /**
      * Pick a login page layout.
@@ -86,7 +86,7 @@ return [
         /**
          * When true, the sidebar content for vertical layouts will not scroll with the rest of the content.
          */
-        'sidebarFixed' => false,
+        'sidebarFixed' => true,
 
         /**
          * When true, horizontal layouts will display the classic top bar on top to free some space when multiple nav items are used.
