9a59294dc226280e0625b89711226a659d700a81
Vahn Gomes <Github@codycody.org>
2023-07-26 12:32:11 -0400

    Update menu item icons in menu_items.blade.php

--- src/resources/views/vendor/backpack/ui/inc/menu_items.blade.php
+++ src/resources/views/vendor/backpack/ui/inc/menu_items.blade.php
@@ -1,6 +1,7 @@
 {{-- This file is used for menu items by any Backpack v6 theme --}}
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i>
+        {{ trans('backpack::base.dashboard') }}</a></li>
 
-<x-backpack::menu-item title="ATIS Audio Files" icon="la la-question" :link="backpack_url('atis-audio-file')" />
-<x-backpack::menu-item title="Airports" icon="la la-question" :link="backpack_url('airport')" />
-<x-backpack::menu-item title="Users" icon="la la-question" :link="backpack_url('user')" />
+<x-backpack::menu-item title="ATIS Audio Files" icon="la la-file-audio" :link="backpack_url('atis-audio-file')" />
+<x-backpack::menu-item title="Airports" icon="la la-plane" :link="backpack_url('airport')" />
+<x-backpack::menu-item title="Users" icon="la la-user" :link="backpack_url('user')" />
