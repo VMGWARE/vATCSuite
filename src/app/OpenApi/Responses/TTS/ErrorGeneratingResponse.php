ca7e605e3ad7e4d23fbea791a94c32c4f0a7d05e
Vahn Gomes <me@vahngomes.dev>
2023-08-04 10:14:09 -0700

    Update TTS response schemas

--- src/app/OpenApi/Responses/TTS/ErrorGeneratingResponse.php
+++ src/app/OpenApi/Responses/TTS/ErrorGeneratingResponse.php
@@ -13,11 +13,20 @@ class ErrorGeneratingResponse extends ResponseFactory implements Reusable
     public function build(): Response
     {
         $response = Schema::object()->properties(
-            Schema::string('status')->example('error'),
-            Schema::string('message')->example('Could not generate ATIS audio file.'),
-            Schema::integer('code')->example(422),
-            Schema::object('data')->nullable()
+            Schema::string('status')
+                ->example('error')
+                ->description('The status of the response indicating the error condition.'),
+            Schema::string('message')
+                ->example('Could not generate ATIS audio file.')
+                ->description('A human-readable message providing additional information about the error.'),
+            Schema::integer('code')
+                ->example(422)
+                ->description('An optional status code for the response. It typically indicates the HTTP status code.'),
+            Schema::object('data')
+                ->nullable()
+                ->description('An optional field for additional data related to the error. It can be null when no data is present.')
         );
+
 
         return Response::create('ErrorGeneratingResponse')
             ->description('Could not generate ATIS audio file.')
--- src/app/OpenApi/Responses/TTS/ErrorRequestConflictResponse.php
+++ src/app/OpenApi/Responses/TTS/ErrorRequestConflictResponse.php
@@ -13,13 +13,25 @@ class ErrorRequestConflictResponse extends ResponseFactory implements Reusable
     public function build(): Response
     {
         $response = Schema::object()->properties(
-            Schema::string('message')->example('This ATIS audio file already exists.'),
-            Schema::integer('code')->example(409),
-            Schema::string('status')->example('error'),
+            Schema::string('message')
+                ->example('This ATIS audio file already exists.')
+                ->description('A human-readable message providing additional information about the error.'),
+            Schema::integer('code')
+                ->example(409)
+                ->description('An optional status code for the response. It typically indicates the HTTP status code.'),
+            Schema::string('status')
+                ->example('error')
+                ->description('The status of the response indicating the error condition.'),
             Schema::object('data')->properties(
-                Schema::string('id')->example('5'),
-                Schema::string('name')->example('KJAX_ATIS_A_260346Z.mp3'),
-                Schema::string('url')->example('/storage/atis/5/KJAX_ATIS_A_260346Z.mp3'),
+                Schema::string('id')
+                    ->example('5')
+                    ->description('The ID of the existing ATIS audio file.'),
+                Schema::string('name')
+                    ->example('KJAX_ATIS_A_260346Z.mp3')
+                    ->description('The filename of the existing ATIS audio file.'),
+                Schema::string('url')
+                    ->example('/storage/atis/5/KJAX_ATIS_A_260346Z.mp3')
+                    ->description('The URL where the existing ATIS audio file can be accessed.'),
             )
         );
 
