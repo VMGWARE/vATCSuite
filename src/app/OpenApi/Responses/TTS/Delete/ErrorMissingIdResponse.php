6bc64a2481fc32b80279d0d1e64c85003f8a5114
Vahn Gomes <me@vahngomes.dev>
2023-08-04 10:24:32 -0700

    Fix error  message in Delete/ErrorMissingIdResponse.php

--- src/app/OpenApi/Responses/TTS/Delete/ErrorMissingIdResponse.php
+++ src/app/OpenApi/Responses/TTS/Delete/ErrorMissingIdResponse.php
@@ -13,14 +13,14 @@ class ErrorMissingIdResponse extends ResponseFactory implements Reusable
     public function build(): Response
     {
         $response = Schema::object()->properties(
-            Schema::string('status')->example('error'),
-            Schema::string('message')->example('You must provide an ATIS ID.'),
-            Schema::integer('code')->example(400),
-            Schema::object('data')->nullable()
+            Schema::string('status')->example('error')->enum('success', 'error')->description('The status of the response indicating the error condition.'),
+            Schema::string('message')->example('You must provide an ATIS ID.')->description('A human-readable message providing additional information about the error.'),
+            Schema::integer('code')->example(400)->description('An optional status code for the response. It typically indicates the HTTP status code.'),
+            Schema::object('data')->nullable()->description('An optional field for additional data related to the error. It can be null when no data is present.')
         );
 
         return Response::create('TTSErrorMissingId')
-            ->description('Missing ATIS ID')
+            ->description('You must provide an ATIS ID.')
             ->content(
                 MediaType::json()->schema($response)
             );
