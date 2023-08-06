bcd71d157b52a7ba3cc9cd7b4ddb719620a8b07b
Vahn Gomes <me@vahngomes.dev>
2023-08-05 19:21:45 -0700

    Refactor TTS response classes

--- src/app/OpenApi/Responses/TTS/Delete/ErrorPasswordProtectedResponse.php
+++ src/app/OpenApi/Responses/TTS/Delete/ErrorPasswordProtectedResponse.php
@@ -13,14 +13,23 @@ class ErrorPasswordProtectedResponse extends ResponseFactory implements Reusable
     public function build(): Response
     {
         $response = Schema::object()->properties(
-            Schema::string('status')->example('error'),
-            Schema::string('message')->example('Password is missing or incorrect.'),
-            Schema::integer('code')->example(401),
-            Schema::object('data')->nullable()
+            Schema::string('status')
+                ->example('error')
+                ->enum('success', 'error')
+                ->description('Status of the response. Can be either "success" or "error".'),
+            Schema::string('message')
+                ->example('Password is missing or incorrect.')
+                ->description('A human-readable message providing additional information about the response.'),
+            Schema::integer('code')
+                ->example(401)
+                ->description('An optional status code for the response. It typically indicates the HTTP status code.'),
+            Schema::object('data')
+                ->nullable()
+                ->description('The data returned by the request.')
         );
 
         return Response::create('ErrorPasswordProtectedResponse')
-            ->description('Password is missing or incorrect.')
+            ->description('Response for ATIS audio file deletion failure due to password protection.')
             ->content(
                 MediaType::json()->schema($response)
             );
