545b6502dec208d3962b6daf1153b5fb568bbf43
Vahn Gomes <me@vahngomes.dev>
2023-09-05 20:22:33 -0400

    Add support for additional options in the TextToSpeech class constructor.

--- src/app/Custom/TextToSpeech.php
+++ src/app/Custom/TextToSpeech.php
@@ -13,6 +13,7 @@ class TextToSpeech
     private mixed $language;
     private mixed $engine;
     private array $API_KEYS;
+    private array $OPTIONS;
 
     /**
      * Create a new TextToSpeech instance.
@@ -20,8 +21,10 @@ class TextToSpeech
      * @param string $text The text to be converted to speech
      * @param string $language The language of the text
      * @param string $engine The TTS engine to be used
+     * @param array $options Additional options for the TTS engine
+     * @throws Exception If the TTS engine is not supported
      */
-    public function __construct($text, $language = 'en-us', $engine = 'VoiceRSS')
+    public function __construct($text, $language = 'en-us', $engine = 'VoiceRSS', $options = [])
     {
         $this->text = $text;
         $this->language = $language;
@@ -32,6 +35,22 @@ class TextToSpeech
             'VoiceRSS' => config('app.voice-rss-key'),
             // 'AnotherTTSAPI' => config('app.another-tts-key'),
         ];
+
+        // Set default options for each engine and merge with provided options
+        $defaultOptions = [
+            'VoiceRSS' => [
+                'format' => 'MP3',
+                'voice' => 'John',
+                'rate' => '16khz_16bit_stereo',
+            ],
+            // 'AnotherTTSAPI' => [ ... ]
+        ];
+
+        if (!array_key_exists($this->engine, $defaultOptions)) {
+            throw new Exception('Unsupported TTS Engine');
+        }
+
+        $this->OPTIONS = array_merge($defaultOptions[$this->engine], $options);
     }
 
     /**
@@ -58,19 +77,28 @@ class TextToSpeech
      */
     private function generateWithVoiceRSS()
     {
-        $ch = curl_init("https://api.voicerss.org/?key=" . $this->API_KEYS['VoiceRSS'] . "&hl=$this->language&c=MP3&v=John&f=16khz_16bit_stereo&src=" . rawurlencode($this->text));
+        $ch = curl_init("https://api.voicerss.org/?key=" . $this->API_KEYS['VoiceRSS'] .
+            "&hl=" . $this->language .
+            "&c=" . $this->OPTIONS['format'] .
+            "&v=" . $this->OPTIONS['voice'] .
+            "&f=" . $this->OPTIONS['rate'] .
+            "&src=" . rawurlencode($this->text)
+        );
+
         curl_setopt($ch, CURLOPT_HEADER, 0);
         curl_setopt($ch, CURLOPT_NOBODY, 0);
         curl_setopt($ch, CURLOPT_TIMEOUT, 5);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 1);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+
         $output = curl_exec($ch);
         $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
         curl_close($ch);
 
         if ($status == 200) {
-            return $output;  // Return the generated audio data
+            return $output;
         }
 
         throw new Exception('VoiceRSS API Error');  // or return null or handle as per your requirements
