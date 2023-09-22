137c6dd7ea6ab5510cb5c2c66ec41aa35c74a7df
Vahn Gomes <me@vahngomes.dev>
2023-09-22 10:08:03 -0400

    feat: Add required fields to GenerateRequestBody

--- src/app/OpenApi/RequestBodies/PDC/GenerateRequestBody.php
+++ src/app/OpenApi/RequestBodies/PDC/GenerateRequestBody.php
@@ -15,27 +15,35 @@ class GenerateRequestBody extends RequestBodyFactory
         $response = Schema::object()->properties(
             Schema::string('callsign')
                 ->example('AAL123')
+                ->required()
                 ->description('The callsign of the flight.'),
             Schema::string('departure')
                 ->example('KJAX')
+                ->required()
                 ->description('The departure airport ICAO code.'),
             Schema::string('arrival')
                 ->example('KDFW')
+                ->required()
                 ->description('The arrival airport ICAO code.'),
             Schema::string('route')
                 ->example('JAX5 J14 MEI J17 MEM J6 PLESS J180 CYN')
+                ->required()
                 ->description('The route of the flight.'),
             Schema::string('aircraft')
                 ->example('B737')
+                ->required()
                 ->description('The aircraft type.'),
             Schema::string('altitude')
                 ->example('FL350')
+                ->required()
                 ->description('The altitude of the flight.'),
             Schema::string('squawk')
                 ->example('1234')
+                ->required()
                 ->description('The squawk code of the flight.'),
             Schema::string('departure_time')
                 ->example('2023-09-15T14:00:00Z')
+                ->required()
                 ->description('The departure time of the flight in ISO 8601 format.'),
             Schema::string('remarks')
                 ->example('VIP onboard, special meal requests')
